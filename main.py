0aa5b11c4d76d0f6c3b17be932230396bdec88c1
rkpls <142883593+rkpls@users.noreply.github.com>
2024-02-28 10:22:45 +0100

    1

--- main.py
+++ main.py
@@ -30,6 +30,7 @@ data_t = 0
 data_h = 0
 data_p = 0
 data_c = 0
+data_d = 0
 
 c = 0
 
@@ -42,9 +43,18 @@ baro = []
 cc = []
 dB = []
 
+passed_oled = 0
+index_list = 0
+passed_sensors = 0
+passed_mqtt = 0
+
 # ---------- PINS ----------
 pin_SDA = 9
 pin_SCL = 8
+
+pin_Air = Pin.OUT(15)
+pin_wled = 16
+
 # ---------- I2C + SENSORS + OLED----------
 i2c = SoftI2C(scl=Pin(pin_SCL), sda=Pin(pin_SDA), freq=100000)
 bh1750 = BH1750(0x23, i2c)
@@ -52,6 +62,7 @@ bme280 = BME280(i2c=i2c)
 ccs = CCS811.CCS811(i2c, addr=0x5a)
 oled = sh1106.SH1106_I2C(128, 64, i2c, Pin(0), 0x3c)
 oled.sleep(False)
+
 # ----------- DEFS ----------
 
 def connect_wifi():
@@ -83,30 +94,27 @@ def read_c(value):
 
 
 def oled_w():
-    passed = 0
-    interval = 100
+    global passed_oled, index_list
+    interval = 3000
     werte = [average(bright), average(temp), average(humid), average(baro), average(cc)]
     print(werte)
-    index = 0
-    while True:
-        time = ticks_ms()
-        if (ticks_diff(time, passed) > interval):
-            wert = werte[index]
-            oled.fill(0)
-            oled.text(str(wert), 8, 6, 1)
-            oled.show()
-            index += 1
-            if index >= len(werte):
-                index = 0
-        passed = time
+    time = ticks_ms()
+    if (ticks_diff(time, passed_oled) > interval):
+        wert = werte[index_list]
+        oled.fill(0)
+        oled.text(str(wert), 8, 24, 1)
+        oled.show()
+        index += 1
+        if index_list >= len(werte):
+            index_list = 0
+    passed_oled = time
 
 
 def sensors_read():
-    global bright, temp, humid, baro, cc, c, data_b, data_t, data_h, data_p, data_c
-    passed = 0
+    global passed_sensors, bright, temp, humid, baro, cc, c, data_b, data_t, data_h, data_p, data_c
     interval = 500
     time = ticks_ms()
-    if (ticks_diff(time, passed) > interval):
+    if (ticks_diff(time, passed_sensors) > interval):
         b = bh1750.measurement
         bright.append(b)
         data_b = b
@@ -121,16 +129,16 @@ def sensors_read():
         baro.append(p)
         if ccs.data_ready():
             c = ccs.eCO2
-        data_c = c
+            if c != 400:                #lesefehler (ausgabe 400) nicht übernehemen
+                data_c = c
         cc.append(c)
-    passed = time
+    passed_sensors = time
 
 def mqtt_send():
-    global CLIENT_ID, MQTT_SERVER, MQTT_TOPIC, data_b, data_t, data_h, data_p, data_c
-    passed = 0
+    global CLIENT_ID, MQTT_SERVER, MQTT_TOPIC, passed_mqtt
     time = ticks_ms()
     interval = 5000
-    if (ticks_diff(time, passed) > interval):
+    if (ticks_diff(time, passed_mqtt) > interval):
         client = MQTTClient(CLIENT_ID, MQTT_SERVER)
         client.connect()
         werte = {
@@ -139,22 +147,18 @@ def mqtt_send():
             'Humid': data_h,
             'Baro': data_p,
             'CCS': data_c,
-            'dB': 1,
+            'dB': data_d,
             }
         dump = json.dumps(werte)
         print(str(dump))
         client.publish(MQTT_TOPIC, dump)
         client.disconnect()
-    passed = time
+    passed_mqtt = time
 
 gc.collect()
 connect_wifi()
 while True:
     sensors_read()
     mqtt_send()
-
-
-
-
-
+    oled_w()
 
